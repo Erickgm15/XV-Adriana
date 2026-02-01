dc7dafb7027adf33384d9b2b3e0b275a8d8bc80b
Erick Garcia <erickgarcia200811@gmail.com>
2026-01-31 18:22:06 -0600

    Buscador terminado

--- familias.js
+++ familias.js
@@ -1,19 +1,65 @@
 const familias = [
-  { nombre: "familia garcia", pases: 5 },
-  { nombre: "familia lopez", pases: 4 },
-  { nombre: "familia martinez", pases: 3 }
+  { nombre: "Delmy Ajuatz", pases: 2 },
+  { nombre: "Jimena Aérevalo", pases: 2 },
+  { nombre: "Nahomy Vásquez", pases: 2 },
+  { nombre: "Julian Ayala", pases: 2},
+  { nombre: "Juan Diego Escobar", pases: 2 },
+  { nombre: "Kamila Esperanza", pases: 2 },
+  { nombre: "Kantyí Martínez", pases: 2 },
+  { nombre: "Keneth Benavídes", pases: 2 },
+  { nombre: "Marvin Flores", pases: 2 },
+  { nombre: "Sofia Martinez", pases: 2 },
+  { nombre: "Ximena Mollinedo", pases: 2 },
+  { nombre: "Isabela Jiatas", pases: 2 },
+  { nombre: "Familia Garía Rivas", pases: 2 },
+  { nombre: "Javier García", pases: 2 },
+  { nombre: "Jorge Rival", pases: 2 },
+  { nombre: "Luis Rivas", pases: 4 },
+  { nombre: "Oscar Rivas", pases: 4 },
+  { nombre: "Byron Escobar", pases: 3 },
+  { nombre: "Luis Zabala", pases: 3 },
+  { nombre: "Familia Vásquez", pases: 4 },
+  { nombre: "Familia Vásquez Kihn", pases: 3 },
+  { nombre: "Familia Mendez Vásquez", pases: 3 },
+  { nombre: "Hugo Vásquez", pases: 1},
+  { nombre: "Oswaldo Vásquez", pases: 2 },
+  { nombre: "Edwin Tejeda", pases: 1},
+  { nombre: "Jorge Benavente", pases: 1 },
+  { nombre: "Javier Figueroa", pases: 1 },
+  { nombre: "Andre Barraza", pases: 1 },
+  { nombre: "Familia Toc Lopez", pases: 3 },
+  { nombre: "Nidia Morales", pases: 4 },
+  { nombre: "Familia Boch Sanchez", pases: 3 },
+  { nombre: "Vinicio Irias", pases: 1 },
+  { nombre: "Victor Manuel Moran", pases: 1 },
+  { nombre: "Emerson Noriega", pases: 2 },
+
+
+
+
+
 ];
 
 // 📲 Número de WhatsApp (Guatemala)
 const numeroWhatsApp = "50230398945";
 
-function buscarFamilia() {
-  const input = document.getElementById("busqueda").value
+function normalizarTexto(texto) {
+  return texto
     .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
     .trim();
+}
+
+function buscarFamilia() {
+  const inputOriginal = document.getElementById("busqueda").value;
+  const input = normalizarTexto(inputOriginal);
 
   const resultado = document.getElementById("resultado");
-  const familia = familias.find(f => f.nombre === input);
+
+  const familia = familias.find(f =>
+    normalizarTexto(f.nombre) === input
+  );
 
   if (!familia) {
     resultado.innerHTML = "❌ Familia no encontrada";
@@ -28,7 +74,7 @@ function buscarFamilia() {
 
   resultado.innerHTML = `
     ✨ <strong>${familia.nombre.toUpperCase()}</strong><br>
-    Pases admitidos: <strong>${familia.pases}</strong><br>
+    Pases admitidos: <strong>${familia.pases}</strong><br><br>
 
     <a href="${linkWhatsApp}" target="_blank" class="btn-whatsapp">
       💬 Confirmar por WhatsApp
